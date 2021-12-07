380bf70415251ebbf8a740ea4dc717d38bc7ab5f
Pavel Lebedev <pashoktver90@gmail.com>
2021-12-07 13:07:28 +0300

    change input & button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 import {Stars} from './Stars';
 
@@ -14,9 +14,34 @@ function App() {
             <Stars value = {value}
                 chengeValue = {chengeValue}
             />
+            <Form/>
         </div>
     );
 }
 
 export default App;
+type tasksType = Array<string>;
 
+function Form(){
+    const [inputValue, setInputValue] = useState("");
+    const [tasks, setTasks] = useState<tasksType>([]);
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.currentTarget.value);
+    }
+
+    const addTask = () =>{
+        tasks.push(inputValue);
+        setTasks([...tasks])
+        setInputValue('')
+    }
+
+
+    return(
+        <div>
+            <label>text:<input value={inputValue} onChange={onChangeHandler}/></label>
+            <button onClick={addTask}>add</button>
+            {tasks.map(m => <li>{m}</li>)}
+        </div>
+    )
+}
